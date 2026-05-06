b8ff8b83726f20bc3dc9eeab0be086ba44481e56
Ichijojoj <maria.gusseva66@gmail.com>
2026-05-06 13:25:50 +0300

    ref pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,36 +1,37 @@
 import logging
-from lab5_PYSPARK.src.config import AppConfig
-from lab5_PYSPARK.src.spark_manager import SparkManager
-from lab5_PYSPARK.src.sanity_check import SanityChecker
-from lab5_PYSPARK.src.preprocessor import DataPreprocessor
-from lab5_PYSPARK.src.clustering import ClusteringModeler
+from py4j.protocol import Py4JJavaError
+from pyspark.errors import AnalysisException, IllegalArgumentException
+
+from src.config import AppConfig
+from src.spark_config import SparkConfig
+from src.spark_manager import SparkManager
+from src.preprocessor import DataPreprocessor
+from src.clustering import ClusteringModeler
+from src.oracle_manager import OracleManager
 
 
 class MLPipeline:
-    """Оркестратор всего процесса машинного обучения."""
+    """Оркестратор процесса машинного обучения с Oracle интеграцией."""
 
     def __init__(self):
         self.config = AppConfig()
-        self.spark_manager = SparkManager(self.config.app_name)
+        self.spark_config = SparkConfig()
+        self.spark_manager = SparkManager(self.spark_config)
         self.logger = logging.getLogger(self.__class__.__name__)
 
     def run(self):
         spark = self.spark_manager.spark
+        db_manager = OracleManager(spark, self.config)
+
         try:
-            # Sanity Check (WordCount)
-            checker = SanityChecker(spark)
-            checker.run_wordcount()
+            #Выгрузка данных
+            raw_df = db_manager.extract_data()
 
             # данные
             preprocessor = DataPreprocessor(spark, self.config)
-            raw_df = preprocessor.load_data()
             clean_df = preprocessor.clean_data(raw_df)
-
-            # кеш перед пайплайном для ускорения
             clean_df.cache()
-            self.logger.info(f"Размер очищенной выборки: {clean_df.count()} строк.")
 
-            #векторизация и масштабирование
             feature_pipeline = preprocessor.build_feature_pipeline()
             feature_model = feature_pipeline.fit(clean_df)
             ml_df = feature_model.transform(clean_df)
@@ -41,10 +42,25 @@ class MLPipeline:
             modeler.evaluate(ml_df)
             modeler.save_model()
 
-            self.logger.info("Пайплайн успешно завершен!")
+            # Получение DataFrame с предсказаниями
+            predictions_df = modeler.model.transform(ml_df)
 
-        except Exception as e:
-            self.logger.error(f"Критическая ошибка в пайплайне: {e}", exc_info=True)
-            raise
+            #Загрузка результатов
+            db_manager.load_results(predictions_df)
+
+            self.logger.info("✅ Пайплайн успешно завершен!")
+
+        #  конкретные бизнес-сценарии сбоев
+        except Py4JJavaError as db_err:
+            self.logger.critical("Сбой на уровне Java/JDBC взаимодействия (БД недоступна или драйвер не найден).")
+            self.logger.error(str(db_err))
+        except AnalysisException as sql_err:
+            self.logger.critical("Ошибка обработки DataFrame/SQL (отсутствуют колонки или неверный синтаксис).")
+            self.logger.error(str(sql_err))
+        except IllegalArgumentException as arg_err:
+            self.logger.critical("Неверные параметры для модели кластеризации.")
+            self.logger.error(str(arg_err))
+        except ConnectionError as conn_err:
+            self.logger.critical(str(conn_err))
         finally:
             self.spark_manager.stop()
